b60267701e503b5cc28c3bda843b2ba115bbb4c3
Lalith Suresh <suresh.lalith@gmail.com>
2011-11-09 19:57:10 +0100

    Implements multi-threading support

--- crawl.py
+++ crawl.py
@@ -1,12 +1,16 @@
 import urllib2
 import re
 import collections
+import multiprocessing
+import Queue
+from threading import Thread
 
 API_KEY = '1b4218629b50c1159e15a6b8285b90ba'
-ROOT_USER = "lalithsuresh"
+ROOT_USER = "rj"
 BASE_LIMIT = 500
-
-visited = []
+NUM_PROCESSES = 5
+NUM_LEVELS = 3
+NUM_THREADS = 1000
 
 
 def fetch_vertex(user, limit, page):
@@ -23,18 +27,39 @@ def fetch_vertex(user, limit, page):
 
     if (totalpages - page != 0):
         friends += fetch_vertex(user, limit, page + 1)[1] # 2nd element of tuple is list of friends
-        print totalpages
 
     return (degree, friends)
 
 
-bfs_queue = collections.deque()
-bfs_queue.extend([ROOT_USER]) # Start with root user
-count = 0
+def worker_function(nodes_to_visit, degree_queue, friends_queue, visited_list):
+    while len(nodes_to_visit) != 0:
+            node = nodes_to_visit.popleft()
 
-while (len(bfs_queue) != 0):
-  count += 1
-  node_to_visit = bfs_queue.popleft()
-  degree, friends = fetch_vertex(node_to_visit, BASE_LIMIT, 1)
-  print count, node_to_visit, degree
-  bfs_queue.extend(friends)
+            if (node not in visited_list):
+                degree, friends = fetch_vertex(node, BASE_LIMIT, 1)
+                degree_queue.append(degree)
+            
+                print "Visited " + str(node) + " " + str(degree)
+                friends_queue.extend(friends)
+
+
+if __name__ == '__main__':
+      bfs_queue = collections.deque([ROOT_USER])
+      level_count = 0
+      visited = collections.deque()
+
+      while (len(bfs_queue) != 0 and level_count < NUM_LEVELS):
+          level_count += 1 
+          degree_queue = collections.deque()
+          friends_queue = collections.deque()
+          threads = [Thread(target=worker_function, args=(bfs_queue, degree_queue, friends_queue, visited)) for i in xrange(NUM_THREADS)]
+
+          for t in threads:
+              t.start()
+          for t in threads:
+              t.join()
+
+          visited = collections.deque()
+
+          bfs_queue = friends_queue
+          print "LEVEL %s done" %(level_count)
